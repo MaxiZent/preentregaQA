3835cc34b1e1b6528fea7c2d1f6115d521e476a8
TuNombre <maxicenturion10gmail.com>
2025-10-15 00:29:28 -0300

    PruebaTitulo

--- test_login.py
+++ test_login.py
@@ -1,27 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+import time 
 
-def test_login():
-    # Inicializa el navegador
-    driver = webdriver.Chrome()
+driver = webdriver.Chrome ()
+driver.implicitly_wait (5)
 
-    try:
-        driver.get("https://www.saucedemo.com/")
-        
-        time.sleep(2)  # Espera para que la página cargue
-        
-        driver.find_element(By.ID, "user-name").send_keys("standard_user")
-        driver.find_element(By.ID, "password").send_keys("secret_sauce")
-        driver.find_element(By.ID, "login-button").click()
-        
-        time.sleep(2)  # Espera para que la página cargue
-        
-        assert "/inventory.html" in driver.current_url,"No se redirigio al inventario"
-        print("Login exitoso y validado")  
+try:
+    #LOGIN
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(By.ID, "user-name").send_keys("standard_user") #ingresamos usuario
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")    #ingresamos contraseña
+    driver.find_element(By.ID, "login-button").click()
     
-    except Exception as e:
-        print(f"Error en login: {e}")
-        raise   #avisa a pytest que hubo un error
-    finally:
-        driver.quit()
+    time.sleep(2)  #Pausa para ver lo que sucede (solo demo)
+    
+    # VALIDACIÓN: Verifica que el usuario fue redirigido al inventario
+    assert "inventory.html" in driver.current_url, "Login fallido o redirección incorrecta" # Verifica que la URL contiene 'inventory.html'
+    print("✅ Login exitoso")
+    
+except Exception as e:  #Captura cualquier error en el bloque try
+    print(f"Error en login: {e}")
+    raise   #avisa a pytest que hubo un error
+    
+finally:
+    driver.quit() #Cierre limpio: mata la sesión y la ventana
